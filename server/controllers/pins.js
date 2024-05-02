8aebb4bfe1697a7a53f24d712087aa835a4d2f23
Chuboii <favoronyechere@gmail.com>
2024-05-02 12:54:33 +0100

    commit

--- server/controllers/pins.js
+++ server/controllers/pins.js
@@ -1,76 +1,73 @@
-import {
-  createError
-} from "../error/Error.js"
-import Pin from "../models/Pin.js"
+import { createError } from "../error/Error.js";
+import Pin from "../models/Pin.js";
 
 export const createUserPin = async (req, res, next) => {
   try {
-    const {
-      userId,
-      pin,
-      username
-    } = req.body
+    const { userId, pin, username } = req.body;
+
+    if (username === "ezeh3942")
+      return next(createError(401, "user is banned"));
 
     const isPinCreated = await Pin.find({
-      userId
-    })
+      userId,
+    });
 
-    if (isPinCreated.length > 0) return next(createError(403, 'You are not allowed to create a new pin'))
+    if (isPinCreated.length > 0)
+      return next(createError(403, "You are not allowed to create a new pin"));
 
-    const hashedPassword = process.env.GENERATED_HASH + pin
+    const hashedPassword = process.env.GENERATED_HASH + pin;
 
     const userPin = new Pin({
       userId,
       username,
-      pin: hashedPassword
-    })
+      pin: hashedPassword,
+    });
 
-    await userPin.save()
+    await userPin.save();
 
-    res.status(200).json("Successfully created")
+    res.status(200).json("Successfully created");
+  } catch (err) {
+    next(err);
   }
-  catch (err) {
-    next(err)
-  }
-}
+};
 
 export const loginUserPin = async (req, res, next) => {
   try {
-    const {
-      userId,
-      pin
-    } = req.body
+    const { userId, pin } = req.body;
+
+    if (userId === "65f8246072651dd963539ee5")
+      return next(createError(401, "user is banned"));
 
     const getPin = await Pin.find({
-      userId
-    })
+      userId,
+    });
 
-    if (getPin.length === 0) return next(createError(403, 'You have not created a pin'))
+    if (getPin.length === 0)
+      return next(createError(403, "You have not created a pin"));
 
-    const isPinCorrect = getPin[0].pin[0] === (process.env.GENERATED_HASH + pin)
+    const isPinCorrect = getPin[0].pin[0] === process.env.GENERATED_HASH + pin;
 
-    if (isPinCorrect === false) return next(createError(401, 'Pin incorrect'))
+    if (isPinCorrect === false) return next(createError(401, "Pin incorrect"));
 
-    res.status(200).json("Successfully unlocked")
-
+    res.status(200).json("Successfully unlocked");
+  } catch (err) {
+    next(err);
   }
-  catch (err) {
-    next(err)
-  }
-
-}
+};
 
 export const resetPin = async (req, res, next) => {
   try {
-    const { userId, pin } = req.body
-    
-    const hashedPassword = process.env.GENERATED_HASH + pin
+    const { userId, pin } = req.body;
 
-    await Pin.findOneAndUpdate({ userId }, { $set: { pin: hashedPassword } })
-    
-    res.status(200).json("You've successfully changed your pin")
+    if (userId === "65f8246072651dd963539ee5")
+      return next(createError(401, "user is banned"));
+
+    const hashedPassword = process.env.GENERATED_HASH + pin;
+
+    await Pin.findOneAndUpdate({ userId }, { $set: { pin: hashedPassword } });
+
+    res.status(200).json("You've successfully changed your pin");
+  } catch (err) {
+    next(err);
   }
-  catch (err) {
-    next(err)
-  }
-}
+};
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,94 +1,94 @@
-import { createError } from "../error/Error.js"
-import User from "../models/User.js"
-import passport from "passport"
+import { createError } from "../error/Error.js";
+import User from "../models/User.js";
+import passport from "passport";
 
 export const registerUser = async (req, res, next) => {
-    try {
-        const { email, username, password, photoUrl} = req.body
-        
-        const checkEmail = await User.find({ email })
-        
-        if (checkEmail.length > 0) return next(createError(401, 'Email is already taken'))
-        
-        const newUser = new User({
-            email,
-            username,
-            photoUrl
-        })
+  try {
+    const { email, username, password, photoUrl } = req.body;
 
-        await User.register(newUser, password)
+    const checkEmail = await User.find({ email });
 
-        await newUser.save()
+    if (checkEmail.length > 0)
+      return next(createError(401, "Email is already taken"));
 
-        res.status(200).json({
-            status: 200,
-            message: "user created successfully",
-            user: newUser
-        })
+    if (username === "ezeh3942")
+      return next(createError(401, "user is banned"));
+
+    const newUser = new User({
+      email,
+      username,
+      photoUrl,
+    });
+
+    await User.register(newUser, password);
+
+    await newUser.save();
+
+    res.status(200).json({
+      status: 200,
+      message: "user created successfully",
+      user: newUser,
+    });
+  } catch (err) {
+    console.log(err.message);
+    if (
+      err.message === "A user with the given username is already registered"
+    ) {
+      next(createError(401, "Ooops! username has already been taken"));
+    } else if (
+      err.message ===
+      `E11000 duplicate key error collection: sme-datashare.users index: email_1 dup key: { email: "${req.body.email}" }`
+    ) {
+      next(createError(401, "Ooops! Email has already been taken"));
+    } else if (err.message === "No username was given") {
+      next(createError(400, "Ooops! You must provide a username"));
+    } else if (
+      err.message ===
+      "Users validation failed: email: Path `email` is required."
+    ) {
+      next(createError(400, "Ooops! You must provide an email address"));
+    } else if (err.message === "No password was given") {
+      next(createError(400, "Ooops! You must provide a password"));
     }
-    catch (err) {
-        console.log(err.message)
-        if (err.message === "A user with the given username is already registered") {
-            next(createError(401, "Ooops! username has already been taken"));
-        }
-        else if (err.message === `E11000 duplicate key error collection: sme-datashare.users index: email_1 dup key: { email: "${req.body.email}" }`) {
-            next(createError(401, "Ooops! Email has already been taken"))
-        }
-        else if (err.message === "No username was given") {
-            next(createError(400, "Ooops! You must provide a username"))
-        }
-        else if (err.message === "Users validation failed: email: Path `email` is required.") {
-            next(createError(400, "Ooops! You must provide an email address"))
-        }
-        else if (err.message === "No password was given") {
-            next(createError(400, "Ooops! You must provide a password"))
-        }
-    }
-}
+  }
+};
 
 export const loginUser = (req, res, next) => {
-  
-        passport.authenticate("local", (err, user, info) => {
-          if (err) return next(err)
-      
-          if (!user) return next(createError(401, info))
-      
-          req.logIn(user, (err) => {
-            if (err) return next(err)
+  passport.authenticate("local", (err, user, info) => {
+    if (err) return next(err);
 
-            return res.status(200).json({ success: true, user });
-          })
-            
-        })(req, res, next);
-}
+    if (!user) return next(createError(401, info));
 
+    req.logIn(user, (err) => {
+      if (err) return next(err);
+
+      return res.status(200).json({ success: true, user });
+    });
+  })(req, res, next);
+};
 
 export const getCurrentUser = async (req, res, next) => {
-    try {
-        const { id } = req.body
-        
-        const user = await User.findById(id)
+  try {
+    const { id } = req.body;
 
-        console.log(user)
-        res.status(200).json({
-            success: 200,
-            user
-        })
-    }
-    catch (err) {
-        next(err)
-    }
-}
+    const user = await User.findById(id);
 
+    console.log(user);
+    res.status(200).json({
+      success: 200,
+      user,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
 export const logoutUser = (req, res, next) => {
-    try {
-        req.logout(() => {
-            res.status(200).json("Logged out successfully")
-        });
-    }
-    catch (e) {
-        next(e)
-    }
-}
-
+  try {
+    req.logout(() => {
+      res.status(200).json("Logged out successfully");
+    });
+  } catch (e) {
+    next(e);
+  }
+};
